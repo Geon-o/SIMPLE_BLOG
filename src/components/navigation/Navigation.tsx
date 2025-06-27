a21c3edc6c54d36dc0fc21853abbb854f1a6c3e8
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-06-27 13:56:13 +0900

    chore: react-router-dom, chakra UI 추가

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,20 @@
+import {Card} from "@chakra-ui/react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Card
+        display="flex"
+        height="50px"
+        width="100%"
+        background={"black"}
+        color={"white"}
+        justifyContent="center"
+      >
+        테스트
+      </Card>
+    </div>
+  );
+};
+
+export default Navigation;
