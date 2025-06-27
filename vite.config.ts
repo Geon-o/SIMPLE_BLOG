8bbe4a535d5a483dfb25b755d278338745e32d3c
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-06-27 10:59:32 +0900

    chore: 라이브러리 추가 및 추가설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SIMPLE_BLOG/'
+  base: '/SIMPLE_BLOG/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+    }
+  }
 })
