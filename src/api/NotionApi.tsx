2a284b81a3b262c8a3a40e24a7bf4fe5be597ec2
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-07-08 14:47:06 +0900

    chore: 노션 api 요청 방식 변경

--- src/api/NotionApi.tsx
+++ src/api/NotionApi.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const getNotionData = async () => {
-    const res = await axios.get('https://notion-proxy-api.vercel.app/api/notion')
+    const res = await axios.post('https://notion-proxy-api.vercel.app/api/content', {
+        "databaseId": "21eeca04b9ba80a78b1ef6ac83dc931a"
+    })
 
     return res.data.results;
 };
