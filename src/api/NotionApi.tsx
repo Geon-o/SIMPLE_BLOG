be9e3eb58c860719e8ba034989711ad75476ffbb
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-07-14 09:23:46 +0900

    chore: 프록시 주소 변경으로 인한 url 변경

--- src/api/NotionApi.tsx
+++ src/api/NotionApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function NotionApi() {
     const contentApi = async () => {
-        const res = await axios.post('https://notion-proxy-api.vercel.app/api/content', {
+        const res = await axios.post('https://notion-proxy-api.vercel.app/api/contentList', {
             "databaseId": "21eeca04b9ba80a78b1ef6ac83dc931a"
         })
 
